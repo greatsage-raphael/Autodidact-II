0a246ae0e8956c6d47c01bfcda3781b8789ec15d
Lubwama Collins <56563537+greatsage-raphael@users.noreply.github.com>
2023-01-01 11:38:47 +0300

    Update PageHeading.tsx

--- components/PageHeading.tsx
+++ components/PageHeading.tsx
@@ -1,4 +1,8 @@
-export default function PageHeading({ children }) {
+type Props = {
+    title: string;
+  };
+
+export default function PageHeading({ children } : React.PropsWithChildren<Props>) {
     return (
       <h1 className="text-2xl font-bold leading-7 text-green-300 sm:truncate sm:text-3xl sm:tracking-tight">{children}</h1>
     )
