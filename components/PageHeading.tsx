c4b0454a49ea63f37912d77ef42d3b9f3ea4639c
Lubwama Collins <56563537+greatsage-raphael@users.noreply.github.com>
2023-01-01 11:59:14 +0300

    Update PageHeading.tsx

--- components/PageHeading.tsx
+++ components/PageHeading.tsx
@@ -1,6 +1,6 @@
 export default function PageHeading(props: { children: React.ReactNode }) {
     return (
-      <h1 className="text-2xl font-bold leading-7 text-green-300 sm:truncate sm:text-3xl sm:tracking-tight">{children}</h1>
+      <h1 className="text-2xl font-bold leading-7 text-green-300 sm:truncate sm:text-3xl sm:tracking-tight">{props.children}</h1>
     )
   }
 
