a11af3161effddc03d23d26855b8f3440181b7be
Lubwama Collins <56563537+greatsage-raphael@users.noreply.github.com>
2023-01-01 11:57:19 +0300

    Update Subject.tsx

--- components/Subject.tsx
+++ components/Subject.tsx
@@ -1,8 +1,5 @@
-type Props = {
-    content: any;
-  };
 
-export default function Subject({ children }: React.PropsWithChildren<Props>) {
+export default function Subject(props: { children: React.ReactNode }) {
     return (
       <h3 className="text-2xl font-bold leading-7 text-blue-300 sm:truncate sm:text-3xl sm:tracking-tight">{children}</h3>
     )
